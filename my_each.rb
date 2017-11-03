6bdf7ae3dec5cb3d6a222581885c954047997568
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-03 20:55:40 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,9 @@ def my_each(words) # put argument(s) here
 
   words = ['hi', 'hello', 'bye', 'goodbye']
   counter = 0
-
-
-
+  
+  while counter < words.length
+    yield words[counter]
+    counter = counter + 1
+  end
 end
